fee4f9eca96f53b6f20f80d3ae4b3b5c74975fba
Simone Russo <34507615+simonerusso97@users.noreply.github.com>
2021-10-06 19:35:59 +0200

    Initial commit

---
+++ sampleServer.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
+
+app.post("/temperature", (req, res, next) => {
+    console.log('received data');
+    res.sendStatus(200)
+});
